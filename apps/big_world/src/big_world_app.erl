fe38b57e22490528831d6eb112ea885f180e5a9a
linkh <421698843@qq.com>
2024-06-13 23:26:50 +0800

    add gpb

--- apps/big_world/src/big_world_app.erl
+++ apps/big_world/src/big_world_app.erl
@@ -9,7 +9,21 @@
 
 -export([start/2, stop/1]).
 
+-include("x.hrl").
+
+encode_person() ->
+    Person = #'Person'{name = <<"Alice">>, id = 123, email = <<"alice@example.com">>},
+    x:encode_msg(Person).
+
+decode_person(Bin) ->
+    x:decode_msg(Bin, 'Person').
+
 start(_StartType, _StartArgs) ->
+    io:format("qwewqe 1--------------- ~n"),
+    Bin = encode_person(),
+        io:format("qwewqe 11--------------- ~n"),
+    Persion = decode_person(Bin),
+    io:format("qwewqe 2--------------- ~p~n", [Persion]),
     big_world_sup:start_link().
 
 stop(_State) ->
